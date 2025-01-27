32ea684197c2836e05b506b5193c2dd1a808548c
abhi-afk-dev <abhilash000006@gmail.com>
2025-01-27 10:27:41 +0530

    Main Rock Paper Scissor game

---
+++ rockpaperscissors.py
@@ -0,0 +1,30 @@
+import random
+
+def rps(com,me):
+    if(com==me):
+        return None
+    elif(com=='r' and me=='p'):
+        return True
+    elif(com=='p' and me=='s'):
+        return True
+    elif(com=='s' and me=='r'):
+        return True
+    else:
+        return False
+    
+choice=('r','p','s')
+
+com=random.randint(0,2)
+com=choice[com]
+
+me=input("Enter r, p or s: ")
+win=rps(com,me)
+print(f"you chose {me} computer chose {com}")
+if(win==None):
+    print('Match drawn')
+
+if(win==True):
+    print('You win')
+elif(win==False):
+    print('Computer wins')
+
